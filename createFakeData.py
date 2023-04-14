c4dc03c673960738bdfbd7e31a6e5073c79df1e3
Sepehr Emami <sepehr7890@gmail.com>
2023-04-14 16:09:03 +0200

    fake data now makes books

--- createFakeData.py
+++ createFakeData.py
@@ -1,14 +1,16 @@
 import os 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book_store.settings")
 from random import randint
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book_store.settings")
+
 import django
 django.setup()
 
 from faker import factory, Faker
 from users.models import * 
-from library.models import Book
+from library.models import Book, Warehousebook
 from basket.models import Basket
 from warehouse.models import Warehouse
+
 from model_bakery.recipe import Recipe, foreign_key
 
 fake = Faker()
@@ -20,10 +22,10 @@ for k in range(10):
                   url=fake.url(),)
     
     customer=Recipe(Customer, 
-                name= fake.name(),
-                address= fake.address(), 
-                phone=fake.phone_number(), 
-                email=fake.email(),)
+                    name= fake.name(),
+                    address= fake.address(), 
+                    phone=fake.phone_number(), 
+                    email=fake.email(),)
     
     publisher=Recipe(Publisher, 
                     address=fake.address(),
@@ -31,26 +33,34 @@ for k in range(10):
                     phone=fake.phone_number(),
                     url=fake.url(),)
     
+    #======================================
+
     warehouse=Recipe(Warehouse, 
                     code=fake.unique.random_int(), 
                     phone=fake.phone_number(),
                     address=fake.address(),)
     
     book=Recipe(Book, 
-                isbn=fake.unique.isbn10(),
-                title=fake.name(),
-                year= fake.past_date(),
-                price=randint(1, 1000),
-                author=foreign_key(author),
-                publisher=foreign_key(publisher),
-                warehouse=foreign_key(warehouse)
+                    isbn=fake.unique.isbn10(),
+                    title=fake.name(),
+                    year= fake.past_date(),
+                    price=randint(1, 1000),
+                    author=foreign_key(author),
+                    publisher=foreign_key(publisher),
                 )
+    
     
     basket=Recipe(Basket,
                    customer=foreign_key(customer),
                    book=foreign_key(book))
+    
+    warehousebook=Recipe(Warehousebook, 
+                    book=foreign_key(book),
+                    warehouse=foreign_key(warehouse),
+                    count=fake.random_int(),)
     book.make()
-    basket.make()
+    # basket.make()
+    warehouse.make()
     publisher.make()
     customer.make()
     author.make()
--- users/urls.py
+++ users/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path, include
 from . import views
+
+
+url_patterns = [
+    path('customer/', views.get_user_list)
+]
